b5898c273fd6328090b1ceca6bb7d8b5037c4ae5
PedroMartinez <martinezrubio.pedro4@gmail.com>
2025-12-10 20:39:16 +0100

    progreso de js

--- DWEC/TEMA 6/Actividades de Aplicacion/6.23/6-23.js
+++ DWEC/TEMA 6/Actividades de Aplicacion/6.23/6-23.js
@@ -9,8 +9,22 @@ setTimeout(() =>{
     }
     
     document.cookie = "nombre=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
-    document.cookie = "edad=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
     
     console.log(document.cookie);
+
+    setTimeout(() =>{
+
+        if(document.cookie.trim() !== ""){
+            document.write("Existen cookies: "+document.cookie+"<br>");
+        }else{
+            document.write("No existe ninguna cookie");
+        }
+        
+        document.cookie = "nombre=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+        document.cookie = "edad=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+        
+        console.log(document.cookie);
+
+    }, 10000);
 
 }, 10000);
---
+++ DWEC/TEMA 6/Actividades de Aplicacion/6.24/6-24.js
@@ -0,0 +1,2 @@
+let filas = prompt("Introduce la cantidad de filas que tenga la columna");
+let columnas = prompt();
