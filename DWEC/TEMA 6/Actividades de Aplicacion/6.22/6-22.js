e1e62a1c33634fc64839a6862e8c20055f37289f
PedroMartinez <martinezrubio.pedro4@gmail.com>
2025-12-10 16:43:13 +0100

    progreso en los ejercicios de js T6

--- DWEC/TEMA 6/Actividades de Aplicacion/6.22/6-22.js
+++ DWEC/TEMA 6/Actividades de Aplicacion/6.22/6-22.js
@@ -3,4 +3,4 @@ let tiempoEnSegundos = 60*30;
 let nombre = prompt("Introduce tu nombre");
 let edad = prompt("Introduce tu edad");
 
-document.cookie = `nombre=${nombre}; edad=${edad};max-age=${tiempoEnSegundos}`;
+document.write("Información de la cookie: "+(document.cookie = `nombre=${nombre}; edad=${edad};max-age=${tiempoEnSegundos}`));
---
+++ DWEC/TEMA 6/Actividades de Aplicacion/6.23/6-23.js
@@ -0,0 +1,16 @@
+document.cookie = 'nombre=antonio; path=/';
+
+
+setTimeout(() =>{
+    if(document.cookie.trim() !== ""){
+        document.write("Existen cookies: "+document.cookie);
+    }else{
+        document.write("No existe ninguna cookie");
+    }
+    
+    document.cookie = "nombre=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+    document.cookie = "edad=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+    
+    console.log(document.cookie);
+
+}, 10000);
