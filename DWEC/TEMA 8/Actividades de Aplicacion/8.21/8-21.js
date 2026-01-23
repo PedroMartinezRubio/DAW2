fcda2f991002803c3baadef598d496335e308f7e
PedroMartinez <martinezrubio.pedro4@gmail.com>
2026-01-23 15:44:02 +0100

    Ejercicios de aplicacion T8

---
+++ DWEC/TEMA 8/Actividades de Aplicacion/8.21/8-21.js
@@ -0,0 +1,6 @@
+//Error en tiempo de ejecución
+
+//let a = 5;
+let b = 4;
+let resultado = a + b;
+console.log(resultado);
---
+++ DWEC/TEMA 8/Actividades de Aplicacion/8.22/8-22.js
@@ -0,0 +1,2 @@
+//elabora un algoritmo en el que pueda verse funcionando cada uno de los tres tipos de errores que pueden darse en una aplicacion web: 
+//error, exception y aviso
